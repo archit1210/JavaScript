a1ade59020aa463b5f3bd816599fa0381af536b3
archit1210 <architlokhande72@gmail.com>
2023-10-04 01:20:17 +0530

    variables in JS

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 4145
+let accountEmail = "archit1210gmail.com"
+var accountPassword = "1234"
+accountCity = "Mumbai"
+
+//accountId = 15
+accountEmail = "archit12102003gmail.com"
+accountPassword = "12345"
+accountCity = "Delhi"
+
+console.table([accountId,accountPassword,accountEmail,accountCity]);
+
+/*
+Prefer not to use var 
+because of functional and block of scope
+*/
