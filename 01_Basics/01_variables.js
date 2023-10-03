3ba2b68444eb0d8d38f6f798363f0dfb40c47f70
archit1210 <architlokhande72@gmail.com>
2023-10-04 01:30:47 +0530

    variables updated

--- 01_Basics/01_variables.js
+++ 01_Basics/01_variables.js
@@ -2,15 +2,16 @@ const accountId = 4145
 let accountEmail = "archit1210gmail.com"
 var accountPassword = "1234"
 accountCity = "Mumbai"
-
+let accountState;
 //accountId = 15
 accountEmail = "archit12102003gmail.com"
 accountPassword = "12345"
 accountCity = "Delhi"
 
-console.table([accountId,accountPassword,accountEmail,accountCity]);
+console.table([accountId,accountPassword,accountEmail,accountCity,accountState]);
 
 /*
 Prefer not to use var 
 because of functional and block of scope
+and default value of a variable is undefined
 */
