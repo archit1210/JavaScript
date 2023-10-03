e5e70fb670887ffbd217466bf29e217ccdca5721
archit1210 <architlokhande72@gmail.com>
2023-10-04 01:45:13 +0530

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict";// treat all JS code as a newer version 
+
+//int or number
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => not yet defined
+//symbol => unique
+
+//objects
+
+let name = "Archit"
+let age = 19
+let isvalid = false
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isvalid);
