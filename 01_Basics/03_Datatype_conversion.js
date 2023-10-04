8f2b748d7307529b9dd26be3f58a28021fa8e28f
archit1210 <architlokhande72@gmail.com>
2023-10-05 00:20:56 +0530

    data-type conversion

--- 01_Basics/03_Datatype_conversion.js
+++ 01_Basics/03_Datatype_conversion.js
@@ -1,27 +1,54 @@
-let score = "Archit"
-console.log(typeof score);
-let value = Number(score)
+// let score = "Archit"
+// console.log(typeof score);
+// let value = Number(score)
 
-console.log(value);
-console.log(typeof value);
+// console.log(value);
+// console.log(typeof value);
 
-//"33" => 33
-//"33abc" => NaN
-// true => 1; false => 0
+// //"33" => 33
+// //"33abc" => NaN
+// // true => 1; false => 0
 
-let isLoggedIn = "Archit"
+// let isLoggedIn = "Archit"
 
-let booleanIsLogged = Boolean(isLoggedIn);
-console.log(typeof booleanIsLogged);
-console.log(booleanIsLogged);
+// let booleanIsLogged = Boolean(isLoggedIn);
+// console.log(typeof booleanIsLogged);
+// console.log(booleanIsLogged);
 
-// 1 => true; 0 => false;
-// "" => false;
-// "archit" => true;
+// // 1 => true; 0 => false;
+// // "" => false;
+// // "archit" => true;
 
-let num1 = 39
+// let num1 = 39
 
-let stringNumber = String(num1)
+// let stringNumber = String(num1)
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// //******* OPERATIONS IN JAVASCRIPT ********/
+
+let x = 3
+let negative = -x
+//console.log(negative);
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2/2)
+console.log(2**2)
+
+let str1 = "Hello"
+let str2 = " Archit"
+let str3 = str1 + str2;
+
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + "2");
+console.log(1 + 2 + "2");
+
+// string is coming first then string will be printed and nothing is treated a their respective datatype all the portion is treated as string 
+// If number is coming first and then string is then number operation is done first then string is printed
