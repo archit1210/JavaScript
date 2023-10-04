29c54e51a965f4877dc190a782d7873a55e43c07
archit1210 <architlokhande72@gmail.com>
2023-10-05 00:10:37 +0530

    data-type conversion

---
+++ 01_Basics/03_Datatype_conversion.js
@@ -0,0 +1,27 @@
+let score = "Archit"
+console.log(typeof score);
+let value = Number(score)
+
+console.log(value);
+console.log(typeof value);
+
+//"33" => 33
+//"33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "Archit"
+
+let booleanIsLogged = Boolean(isLoggedIn);
+console.log(typeof booleanIsLogged);
+console.log(booleanIsLogged);
+
+// 1 => true; 0 => false;
+// "" => false;
+// "archit" => true;
+
+let num1 = 39
+
+let stringNumber = String(num1)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
