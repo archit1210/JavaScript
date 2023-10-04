59be45316d463c51df738f7cee4b56e2dee4c6df
archit1210 <architlokhande72@gmail.com>
2023-10-05 00:33:03 +0530

    comparison operators

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,19 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+
+// console.log("2" < 1);
+// console.log("02" > 1);
+// while conversion datatype must be same
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+// ===
+console.log("2" === 2);
+console.log("2" == 2);
+// == checks the string only 
+// === checks the whole string with the datatype too.It is used for strict check
+
