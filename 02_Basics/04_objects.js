6456efb66b16fd0a2188e2227a9e449b692f2a4b
archit1210 <architlokhande72@gmail.com>
2023-10-11 01:08:34 +0530

    json and object destructuring

--- 02_Basics/04_objects.js
+++ 02_Basics/04_objects.js
@@ -53,8 +53,18 @@ console.log(obj3)
 
 // console.log(users[1]);
 
-console.log(Object.keys(tinderUser))
-console.log(Object.values(tinderUser))
-console.log(Object.entries(tinderUser))
+// console.log(Object.keys(tinderUser))
+// console.log(Object.values(tinderUser))
+// console.log(Object.entries(tinderUser))
 
-console.log(tinderUser.hasOwnProperty('isLogged'));
+// console.log(tinderUser.hasOwnProperty('isLogged'));
+
+const course = {
+    coursename : "js in hindi",
+    price : "999",
+    courseInstructor : "hitesh"
+}
+
+const {courseInstructor : Instructor} = course // destructuring of objects in js
+
+console.log(Instructor);
