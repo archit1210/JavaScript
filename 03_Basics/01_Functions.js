d7a8ddfb7b3d48887f42f71feb75908a515692fe
archit1210 <architlokhande72@gmail.com>
2023-10-13 00:37:37 +0530

    Functions intro

---
+++ 03_Basics/01_Functions.js
@@ -0,0 +1,22 @@
+function sayMyName(){
+    console.log("Archit");
+}
+
+// sayMyName()
+
+// function add(num1,num2){
+//     // let ans = num1 + num2
+//     // return ans
+
+//     return num1 + num2
+// }
+
+// const answer = add(3,4)
+// console.log(answer)
+
+function loginUser(username){
+    return `${username} just logged in`
+}
+
+const user = loginUser("Archit")
+console.log(user);
