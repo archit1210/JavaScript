8333ff01e2b8e6a0c81faa17ea95cf441dcc1969
archit1210 <architlokhande72@gmail.com>
2023-10-21 19:10:09 +0530

    while and do while loop is js

---
+++ 05_Iterations/two.js
@@ -0,0 +1,19 @@
+// let index  = 0
+// while(index <= 10){
+//     console.log(`Value of index is ${index}`);
+//     index += 2
+// }
+
+// let arr = [1,2,3,4,5,8,7,'archit']
+// let i = 0;
+// while(i < arr.length){
+//     console.log(`Value is ${arr[i]}`);
+//     i++
+// }
+
+let score = 11
+
+do{
+    console.log(`Score is ${score}`);
+    score++
+}while(score <= 10);
